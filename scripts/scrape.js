cfc640043be915894773f83c265ba5ce7c9cc9ec
Ridvan Cekmez <ridvanceko@gmail.com>
2019-12-08 23:08:34 -0700

    router established

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -3,24 +3,24 @@ var cheerio = require("cheerio");
 
 var scrape = function (cb)
 
-request("https://www.cnn.com/", function (err, res, body){
+request("https://www.bbc.com/", function (err, res, body){
   var $ = cheerio.load(body);
   var articles = [];
 
   $(".cd").each(function(i, element){
 
-    var head = $(this).children("cd__headline").text().trim();
+    var head = $(this).children("media__title").text().trim();
 
     //couldnt find it <p> tag inside of the inspect
-    // var sum = $(this).children(".")
+    var sum = $(this).children(".media__summary").text().trim();
 
     if(head){
       var headNeat = head.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
-      // var sumNeat = sum.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+      var sumNeat = sum.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
 
       var dataToAdd = {
         headLine: headNeat,
-        // summary: sumNeat
+        summary: sumNeat
       };
       articles.push(dataToAdd);
     }
